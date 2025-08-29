c69f600bb384cea120d63a9ab7c1991099a9d69f
Sree1710 <sreelekshmisl1710@gmail.com>
2025-08-29 13:26:31 +0530

    CORS added

--- tripwise/settings.py
+++ tripwise/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +59,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # your React app
+]
+
+CORS_ALLOWED_ORIGINS += ["http://127.0.0.1:3000"]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'tripwise.urls'
 
